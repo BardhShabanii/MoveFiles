76a861b2404571a177d1f3e2d525f1947f32199d
BardhShabanii <bardhi-0105@hotmail.com>
2022-11-04 23:07:28 +0100

    commented some code that is not needed at the moment

--- main.py
+++ main.py
@@ -2,21 +2,19 @@ import os
 import shutil
 
 source = input("Please give directory source path:")
-dest = input("Please give directory destination path:")
+# dest = input("Please give directory destination path:")
 
-img_folder = r"C:\Users\bardh\Desktop\ick-python-advanced\mini projects\MoveFiles\Dest Path\Images"
-vid_folder = r"C:\Users\bardh\Desktop\ick-python-advanced\mini projects\MoveFiles\Dest Path\Videos"
-aud_folder = r"C:\Users\bardh\Desktop\ick-python-advanced\mini projects\MoveFiles\Dest Path\Audios"
-software_folder = r"C:\Users\bardh\Desktop\ick-python-advanced\mini projects\MoveFiles\Dest Path\Software"
-doc_folder = r"C:\Users\bardh\Desktop\ick-python-advanced\mini projects\MoveFiles\Dest Path\Documents"
-other_folder = r"C:\Users\bardh\Desktop\ick-python-advanced\mini projects\MoveFiles\Dest Path\Other"
+
+img_folder = r"C:\Users\Lenovo\bardhipython\ick-python-advanced\mini projects\MoveFiles\Dest Path\Images"
+vid_folder = r"C:\Users\Lenovo\bardhipython\ick-python-advanced\mini projects\MoveFiles\Dest Path\Videos"
+aud_folder = r"C:\Users\Lenovo\bardhipython\ick-python-advanced\mini projects\MoveFiles\Dest Path\Audios"
+software_folder = r"C:\Users\Lenovo\bardhipython\ick-python-advanced\mini projects\MoveFiles\Dest Path\Software"
+doc_folder = r"C:\Users\Lenovo\bardhipython\ick-python-advanced\mini projects\MoveFiles\Dest Path\Documents"
+other_folder = r"C:\Users\Lenovo\bardhipython\ick-python-advanced\mini projects\MoveFiles\Dest Path\Other"
 
 files = os.listdir(source)  # takes list of files in folder
 for file in files:  # takes each file of folder one by one
     suffix = (os.path.splitext(file))[-1]  # suffixes are split from source
-    print(suffix)
-    print('asd')
-    print(file)
     if suffix == '.jpg' or suffix == '.jpeg':
         shutil.move(f"{source}/{file}", img_folder)
 
@@ -36,10 +34,10 @@ for file in files:  # takes each file of folder one by one
         shutil.move(f"{source}/{file}", other_folder)
 
     # new_path = shutil.move(f"{source}/{file}", dest)  # moves files to new folder, takes source from user input, file from organised files
-    # print(new_path)
+        # print(new_path)
 
-entry = input("Please give your source directory path here: ")
-source = os.listdir(entry)
+# entry = input("Please give your source directory path here: ")
+# source = os.listdir(entry)
 # entries = os.listdir(r"C:\Users\bardh\Desktop\ick-python-advanced\mini projects\MoveFiles")
 
 print(source)
